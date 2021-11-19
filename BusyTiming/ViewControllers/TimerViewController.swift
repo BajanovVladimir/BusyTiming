051fa7affbdcf7038658b929cf5a1ae7c8ddc1d2
BajanovVladimir <bajanov_v_s@mail.ru>
2021-11-19 09:08:09 +0400

    Creation of activity with memorization of time

--- BusyTiming/ViewControllers/TimerViewController.swift
+++ BusyTiming/ViewControllers/TimerViewController.swift
@@ -12,9 +12,10 @@ class TimerViewController: UIViewController {
     var timerCounting = false
     var timer = MainTimerUse()
     var i = 0
-    var activity = ActivitiesModel()
+    let activity = ActivitiesModel()
     var selectedIndex = 0
     
+    @IBOutlet weak var activitiesTableView: UITableView!
     @IBOutlet weak var startStopButton: UIButton!
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var saveTimerLabel: UILabel!
@@ -23,32 +24,59 @@ class TimerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        selectedIndex = activity.selectedIndex
-        timer.totalTimeInt = activity.activity[selectedIndex].time
-        saveTimerLabel.text = timer.conversionOfTimeFromSecondsToString(timer.totalTimeInt)
-        activityLabel.text = activity.activityName
-        setDisignStartBattons()
-        setDisignSaveTimeButtonHide()
+        timer.timerResult = 0
+        timer.displayTimerDelegat = self
+        timerCounting = true
+        timer.timerOn()
     }
     
+   
     func setDisignStopBattons () {
-        startStopButton.setTitle("◼︎", for:.normal)
-        startStopButton.setTitleColor(UIColor.red, for: .normal)
+    
     }
     
     func setDisignSaveTimeButtonHide () {
-        saveTimerButton.setTitle("Save", for: .normal)
-        saveTimerButton.setTitleColor(UIColor.gray, for: .normal)
+       
     }
     
     func setDisignSaveTimeButtonShow () {
-        saveTimerButton.setTitle("Save", for: .normal)
-        saveTimerButton.setTitleColor(UIColor.green, for: .normal)
+      
     }
     
     func setDisignStartBattons () {
-        startStopButton.setTitle("▶︎", for:.normal)
-        startStopButton.setTitleColor(UIColor.green, for: .normal)
+     
+    }
+    func  saveNewActiviti(_ name:String?,_ time: Int) {
+        guard let newName = name else {
+            return
+        }
+        self.activity.addActivity(nameOfActivity: newName, time: time)
+        self.activitiesTableView.reloadData()
+    }
+    
+    @IBAction func  addActivityButtonPressed(_ sender: UIButton){
+        let time = self.timer.timerResult
+        let alertController = UIAlertController(title: "Enter the name of  the affair", message: "", preferredStyle: .alert)
+        alertController.addTextField { (textField) in
+            textField.placeholder = "New affair"
+        }
+        let alertAction1 = UIAlertAction(title: "Create&Restart timer", style:.default) {
+            (alert) in
+            self.saveNewActiviti(alertController.textFields?[0].text,time)
+            self.timer.timerResult = 0
+           
+        }
+        let alertAction2 = UIAlertAction(title: "Create", style: .cancel) {
+            (alert) in
+            self.saveNewActiviti(alertController.textFields?[0].text,time)
+        }
+        let alertAction3 = UIAlertAction(title: "Cancel", style:.destructive) {
+            (alert) in
+        }
+        alertController.addAction(alertAction1)
+        alertController.addAction(alertAction2)
+        alertController.addAction(alertAction3)
+        present(alertController, animated: true, completion: nil)
     }
 
     @IBAction func startStopButtonPress(_ sender: UIButton) {
@@ -60,7 +88,7 @@ class TimerViewController: UIViewController {
             timer.calculateTotalTime()
         } else {
             setDisignStopBattons()
-            timer.timerResultValue = 0
+            timer.timerResult = 0
             timer.displayTimerDelegat = self
             timerCounting = true
             timer.timerOn()
@@ -69,15 +97,48 @@ class TimerViewController: UIViewController {
     
     @IBAction func saveTimeButtonPressed(_ sender: UIButton) {
         if !timerCounting {
-            setDisignSaveTimeButtonHide()
             saveTimerLabel.text = timer.totalTimeString
             activity.activity[selectedIndex].time = timer.totalTimeInt
         }
     }
 }
 
-extension    TimerViewController: DisplayTimerProtocol {
+extension    TimerViewController: DisplayTimerProtocol, UITableViewDataSource, UITableViewDelegate {
+   
     func displayTimer(result: String) {
-            timerLabel.text  = result
+        timerLabel.text  = result
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return activity.activity.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ActivitiesCell", for: indexPath)
+        let currentList = activity.activity[indexPath.row]
+        let currentName = currentList.name
+        let currentTime = currentList.time
+        let curentTimeString = timer.conversionOfTimeFromSecondsToString(currentTime)
+        cell.textLabel?.text = currentName + "     " + curentTimeString
+        if currentList.isSelected == true {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        return cell    }
+    
+     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+   
+        if editingStyle == .delete {
+            activity.deleteActivity(index: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        activity.changeState(index: indexPath.row)
+        tableView.reloadData()
+    }
+    
 }
